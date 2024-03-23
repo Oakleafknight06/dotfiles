46221e26bfefc9b518b0e88e5bfc787e945188d6
Oakleafknight06 <65838823+Oakleafknight06@users.noreply.github.com>
2024-03-23 23:03:06 +0800

    fish and gits

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,5 +1,10 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+  zoxide init fish | source
+  starship init fish | source
+  atuin init fish | source
+  abbr -a -- cd z
+  abbr -a -- ls lsd
+  abbr -a -- vim nvim
+  abbr -a -- cat bat
 end
-zoxide init fish | source
-starship init fish | source
