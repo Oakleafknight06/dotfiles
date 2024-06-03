2915ee22016d7935b5ac358da9c0ed809d83a258
Oakleafknight06 <65838823+Oakleafknight06@users.noreply.github.com>
2024-06-03 18:30:10 +0800

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,7 +1,7 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+  # Commands to run in interactive sessions can go here
   zoxide init fish | source
-  starship init fish | source
+  # starship init fish | source
   atuin init fish | source
   abbr -a -- cd z
   abbr -a -- ls eza
