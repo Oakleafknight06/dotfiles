87cfdb6d63a63c2955ba28a176a1600258d0bc73
Oakleafknight06 <65838823+Oakleafknight06@users.noreply.github.com>
2024-06-05 16:43:58 +0800

    Update .config/fish/config.fish Update .config/fish/fish_variables

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,10 +1,9 @@
 if status is-interactive
   # Commands to run in interactive sessions can go here
   zoxide init fish | source
-  # starship init fish | source
   atuin init fish | source
   abbr -a -- cd z
-  abbr -a -- ls eza
+  abbr -a -- ls "eza --icons"
   abbr -a -- vim nvim
   abbr -a -- cat bat
 end
