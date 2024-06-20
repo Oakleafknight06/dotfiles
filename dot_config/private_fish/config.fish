20f5fc06fd7747d538ee33b36df84b333af88c4f
Oakleafknight06 <65838823+Oakleafknight06@users.noreply.github.com>
2024-06-20 11:47:20 +0800

    Remove .config/fish/completions/.keep Remove .config/fish/conf.d/.keep Remove .config/fish/config.fish Remove .config/fish/fish_variables Remove .config/fish/functions/.keep

--- dot_config/private_fish/config.fish
+++
@@ -1,9 +0,0 @@
-if status is-interactive
-  # Commands to run in interactive sessions can go here
-  zoxide init fish | source
-  atuin init fish | source
-  abbr -a -- cd z
-  abbr -a -- ls eza
-  abbr -a -- vim nvim
-  abbr -a -- cat bat
-end
