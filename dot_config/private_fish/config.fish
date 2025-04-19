edc26a6e9238915c8068bf49cefb7f91de2128f7
Oakleafknight06 <65838823+Oakleafknight06@users.noreply.github.com>
2025-04-18 21:48:45 -0400

    Remove deprecated portion from brewfile

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,12 +1,19 @@
 if status is-interactive
   # Commands to run in interactive sessions can go here
+  #
+  # Initializing better cd and better shell history
   zoxide init fish | source
   atuin init fish | source
+  # better ls and better ls with options
+  abbr -a -- ls 'eza -a'
+  abbr -a -- ll 'eza -lah --group-directories-first'
+  # Regular utility switiching
   abbr -a -- cd z
-  abbr -a -- ls eza
   abbr -a -- vim nvim
   abbr -a -- cat bat
-  # 'td' for TailDrop
+  # Makes dotfile setup easier
+  abbr -a -- unpackbrew 'brew bundle --file ~/.config/linuxbrew/Brewfile'
+  # 'td' for TailDrop and 'tg' for TailGet
   abbr -a -- td 'tailscale file cp'
   abbr -a -- tg 'tailscale file get ~/Downloads'
 end
